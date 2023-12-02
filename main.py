57815e225dac956c74af5871eea38ff8a3c1d395
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-12-03 03:09:12 +0330

    Prototype diode

--- main.py
+++ main.py
@@ -85,6 +85,30 @@ class Capacitor(Component):
         )
 
 
+import math
+
+
+class Diode(Component):
+    def __init__(self, coeff_in, coeff_out, a: Node, b: Node):
+        super().__init__()
+        self.coeff_in = coeff_in
+        self.coeff_out = coeff_out
+        self.a = a
+        self.b = b
+
+    def apply(self, solver: Solver):
+        solver.add_func(
+            self.a.index,
+            lambda: (math.exp((self.a.value - self.b.value) * self.coeff_in) - 1)
+            * self.coeff_out,
+        )
+        solver.add_func(
+            self.b.index,
+            lambda: -(math.exp((self.a.value - self.b.value) * self.coeff_in) - 1)
+            * self.coeff_out,
+        )
+
+
 class CurrentSource(Component):
     def __init__(self, amps, a: Node, b: Node):
         super().__init__()
@@ -121,10 +145,12 @@ class Circuit:
 c = Circuit()
 v1 = c.new_node("v1")
 v2 = c.new_node("v2")
+v3 = c.new_node("v3")
 
 c.new_component(CurrentSource(1, c.gnd, v1))
 c.new_component(Resistor(5, v1, v2))
-c.new_component(Capacitor(1, v2, c.gnd))
+c.new_component(Capacitor(1, v2, v3))
+c.new_component(Diode(1, 1, v3, c.gnd))
 
 
 solver = c.solve()
@@ -133,13 +159,18 @@ import numpy
 
 for _ in range(10000):
     for _ in range(10):
-        X_prime = solver.jacobian([v1, v2])
+        X_prime = solver.jacobian([v1, v2, v3])
         X_prime_inv = numpy.linalg.inv(X_prime)
-        X = [v1.value, v2.value]
-        f_X = [solver.eval(0), solver.eval(1)]
+        X = [v1.value, v2.value, v3.value]
+        f_X = [solver.eval(0), solver.eval(1), solver.eval(2)]
+        old_x = X
         X = X - numpy.dot(X_prime_inv, f_X)
         v1.value = X[0]
         v2.value = X[1]
+        v3.value = X[2]
+    if not numpy.allclose(old_x, X):
+        raise Exception("Convergence failed!")
     print(v2.value)
     v1.old_value = v1.value
     v2.old_value = v2.value
+    v3.old_value = v3.value
