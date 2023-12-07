cbfa837dea3b2c4dc9e1c1563f047872c02479c9
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-12-07 20:01:42 +0330

    Update bjt

--- components/bjt.py
+++ components/bjt.py
@@ -23,14 +23,9 @@ class Bjt(Component):
         self.beta_f = beta_f
 
     def apply(self, solver: Solver):
-
-        math.exp(
-            (self.base.value - self.emitter.value) * self.coeff_in
-        ) * self.coeff_out * self.coeff_in
-
         solver.add_func(
-            self.emitter.index,
-            lambda: -(
+            self.collector.index,
+            lambda: (
                 (
                     math.exp((self.base.value - self.emitter.value) * self.coeff_in)
                     - math.exp((self.base.value - self.collector.value) * self.coeff_in)
@@ -44,7 +39,7 @@ class Bjt(Component):
             * self.coeff_out,
         )
         solver.add_func(
-            self.collector.index,
+            self.base.index,
             lambda: (
                 (1 / self.beta_f)
                 * (math.exp((self.base.value - self.emitter.value) * self.coeff_in) - 1)
@@ -57,40 +52,33 @@ class Bjt(Component):
             * self.coeff_out,
         )
         solver.add_func(
-            self.base.index,
-            lambda: (
+            self.emitter.index,
+            lambda: -(
                 (
                     math.exp((self.base.value - self.emitter.value) * self.coeff_in)
                     - math.exp((self.base.value - self.collector.value) * self.coeff_in)
                 )
-                - (1 / self.beta_f)
+                + (1 / self.beta_f)
                 * (math.exp((self.base.value - self.emitter.value) * self.coeff_in) - 1)
             )
             * self.coeff_out,
         )
 
         solver.add_deriv(
-            self.emitter.index,
-            self.emitter.index,
-            lambda: math.exp((self.base.value - self.emitter.value) * self.coeff_in)
-            * self.coeff_out
-            * self.coeff_in,
-        )
-        solver.add_deriv(
-            self.emitter.index,
             self.collector.index,
-            lambda: -math.exp((self.base.value - self.collector.value) * self.coeff_in)
+            self.collector.index,
+            lambda: math.exp((self.base.value - self.collector.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in
-            - math.exp((self.base.value - self.collector.value) * self.coeff_in)
+            + math.exp((self.base.value - self.collector.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in
             * (1 / self.beta_r),
         )
         solver.add_deriv(
-            self.emitter.index,
+            self.collector.index,
             self.base.index,
-            lambda: -math.exp((self.base.value - self.emitter.value) * self.coeff_in)
+            lambda: math.exp((self.base.value - self.emitter.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in
             + math.exp((self.base.value - self.collector.value) * self.coeff_in)
@@ -101,17 +89,16 @@ class Bjt(Component):
             * self.coeff_in
             * (1 / self.beta_r),
         )
-
         solver.add_deriv(
             self.collector.index,
             self.emitter.index,
-            lambda: -(1 / self.beta_f)
-            * math.exp((self.base.value - self.emitter.value) * self.coeff_in)
+            lambda: -math.exp((self.base.value - self.emitter.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in,
         )
+
         solver.add_deriv(
-            self.collector.index,
+            self.base.index,
             self.collector.index,
             lambda: -(1 / self.beta_r)
             * math.exp((self.base.value - self.collector.value) * self.coeff_in)
@@ -119,7 +106,7 @@ class Bjt(Component):
             * self.coeff_in,
         )
         solver.add_deriv(
-            self.collector.index,
+            self.base.index,
             self.base.index,
             lambda: (1 / self.beta_f)
             * math.exp((self.base.value - self.emitter.value) * self.coeff_in)
@@ -130,11 +117,29 @@ class Bjt(Component):
             * self.coeff_out
             * self.coeff_in,
         )
-
         solver.add_deriv(
             self.base.index,
             self.emitter.index,
+            lambda: -(1 / self.beta_f)
+            * math.exp((self.base.value - self.emitter.value) * self.coeff_in)
+            * self.coeff_out
+            * self.coeff_in,
+        )
+
+        solver.add_deriv(
+            self.emitter.index,
+            self.collector.index,
+            lambda: -math.exp((self.base.value - self.collector.value) * self.coeff_in)
+            * self.coeff_out
+            * self.coeff_in,
+        )
+        solver.add_deriv(
+            self.emitter.index,
+            self.base.index,
             lambda: -math.exp((self.base.value - self.emitter.value) * self.coeff_in)
+            * self.coeff_out
+            * self.coeff_in
+            - math.exp((self.base.value - self.collector.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in
             + (1 / self.beta_f)
@@ -143,22 +148,12 @@ class Bjt(Component):
             * self.coeff_in,
         )
         solver.add_deriv(
-            self.base.index,
-            self.collector.index,
-            lambda: math.exp((self.base.value - self.collector.value) * self.coeff_in)
-            * self.coeff_out
-            * self.coeff_in,
-        )
-        solver.add_deriv(
-            self.base.index,
-            self.base.index,
+            self.emitter.index,
+            self.emitter.index,
             lambda: math.exp((self.base.value - self.emitter.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in
-            + math.exp((self.base.value - self.collector.value) * self.coeff_in)
-            * self.coeff_out
-            * self.coeff_in
-            - (1 / self.beta_f)
+            + (1 / self.beta_f)
             * math.exp((self.base.value - self.emitter.value) * self.coeff_in)
             * self.coeff_out
             * self.coeff_in,
--- main.py
+++ main.py
@@ -6,14 +6,16 @@ c = Circuit()
 v1 = c.new_var("v1")
 v2 = c.new_var("v2")
 v3 = c.new_var("v3")
+v4 = c.new_var("v4")
 i = c.new_var("i")
 
 from components.bjt import Bjt
 
-c.new_component(VoltageSource(3, c.gnd, v1, i))
+c.new_component(VoltageSource(1, c.gnd, v1, i))
 c.new_component(Resistor(100, v1, v2))
 c.new_component(Resistor(100, v1, v3))
-c.new_component(Bjt(1 / 0.026, 1e-14, 10, 250, v2, v3, c.gnd))
+c.new_component(Bjt(1 / 0.026, 1e-14, 10, 250, v2, v3, v4))
+c.new_component(Resistor(100, v4, c.gnd))
 
 
 solver = c.solve()
